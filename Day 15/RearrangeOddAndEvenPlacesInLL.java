2aee91f7e79b6e996278eb6da7a8b02a723ac03a
Amiya <122077589+AmiyaNix@users.noreply.github.com>
2024-08-20 16:19:29 +0530

    Create RearrangeOddAndEvenPlacesInLL.java

---
+++ Day 15/RearrangeOddAndEvenPlacesInLL.java
@@ -0,0 +1,45 @@
+/****************************************************************
+
+ Following is the Linked list node structure already written:-
+
+ class Node
+ {
+     public int data;
+     public Node next;
+
+     Node()
+     {
+         this.data = 0;
+         this.next = null;
+     }
+
+     Node(int data)
+     {
+         this.data = data;
+         this.next = null;
+     }
+ }
+
+ *****************************************************************/
+
+
+public class Solution {
+    public static Node oddEvenList(Node head) {
+        if(head==null || head.next==null){
+            return head;
+        }
+        Node odd = head;
+        Node even = head.next;
+        Node evenHead = head.next;
+        while(even!=null && even.next!=null){
+            odd.next = odd.next.next;
+            even.next = even.next.next;
+
+            odd = odd.next;
+            even = even.next;
+        }
+        odd.next = evenHead;
+
+        return head;
+    }
+}
